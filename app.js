0a6f09b3613f74573c48cd24df37aabe1f8d419b
Dominic52 <dongyuanyang@hotmail.com>
2020-04-21 14:35:11 +1200

    capital P

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 let app = express();
-app.use(express.static("public"));
+app.use(express.static("Public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
