0c5d563df43e407e2f4082fb7ba5a8675458f73a
Dominic52 <dongyuanyang@hotmail.com>
2020-04-21 14:25:33 +1200

    predeploy to heroku

--- app.js
+++ app.js
@@ -11,14 +11,44 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-    console.log(req);
     let email = req.body.inputEmail;
     let fname = req.body.firstName;
     let lname = req.body.lastName;
-    console.log(email, fname, lname);
-    res.send(email);
-})
 
-app.listen(3000, function(){
+    const data = {
+        members:[{
+            email_address: email,
+            status: "subscribed",
+            merge_fields:{
+                FNAME: fname,
+                LNAME: lname
+            }
+        }]
+    };
+
+    const jsonData = JSON.stringify(data);
+    const endpoint = "https://us4.api.mailchimp.com/3.0/lists/a4fb8af0fe";
+    const options = {
+        method: 'POST',
+        auth: "dominic:7517b877c9bd8b05e9f2fe9d7788bd5a-us4"
+    }
+
+    const request = https.request(endpoint, options, function(response){
+        if (response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+    });
+
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
     console.log("Port 3000");
-})
+});
